f3bf0e9c5398a7fd37e6760bc13ed925ff866231
SouFiane-SinCara <sfnweb2@gmail.com>
2025-07-30 14:34:20 +0200

    initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# servizio_x
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/servizio_x/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.servizio_x
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
